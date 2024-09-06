81ac6279dd638ae2d1b9cb104585dd9d62b26b43
baggariSiva <narayana@softsuave.com>
2024-09-06 16:14:14 +0530

    Initial Commit for Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@mui/material";
+import "./App.css";
+import ProductGallery from "./Components/ProductGallery";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography sx={{fontSize:'36px',fontWeight:'700'}}>Photo Gallery</Typography>
+      <ProductGallery />
     </div>
   );
 }
